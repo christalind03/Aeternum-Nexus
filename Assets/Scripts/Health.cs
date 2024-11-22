608149eace72fe5b58b419877159c3bfb2dc453a
ItsAlexMedal <cas9400@gmail.com>
2024-11-21 17:13:22 -0800

    Many alpha prep changes
    changes:
    -removed playerHealthScript
    -made boss health bar only appear when the boss is damaged
    -changed level select background to only show one option

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -10,6 +10,8 @@ public class Health : MonoBehaviour
     [SerializeField] private float _currentHealth; // Temporarily serialize the field to ensure health-related functionalities work properly
 
     [SerializeField] private Image _HealthBarFIll;
+    [SerializeField] private Image _HealthBarBack;
+
     public string sceneToReload;
     public float MaximumHealth { get { return _maximumHealth; } }
     public float CurrentHealth { get { return _currentHealth; } }
@@ -17,6 +19,14 @@ public class Health : MonoBehaviour
     private void Start()
     {
         _currentHealth = _maximumHealth;
+        if(gameObject.name == "boss1")
+        {
+            _HealthBarFIll.color = new Color32(1, 1, 1, 0);
+            _HealthBarBack.color = new Color32(1, 1, 1, 0);
+
+        }
+
+
     }
 
     public void AddHealth(float healthPoints)
@@ -33,6 +43,13 @@ public class Health : MonoBehaviour
     public void RemoveHealth(float healthPoints)
     {
         _currentHealth -= healthPoints;
+        if (gameObject.name == "boss1")
+        {
+            float fillAmount = _currentHealth / _maximumHealth;
+            _HealthBarFIll.color = new Color32(254,0,0,255);
+            _HealthBarBack.color = new Color32(118, 0, 7, 255);
+
+        }
 
         // Zero means that the current object has no more health.
         if (_currentHealth <= 0)
